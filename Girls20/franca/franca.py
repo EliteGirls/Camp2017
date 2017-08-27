d76c9770e8c17330444a29c874326ec5a1d02605
EliteGirls <31381682+EliteGirls@users.noreply.github.com>
2017-08-27 09:18:48 +0000

    Add files via upload

---
+++ Girls20/franca/franca.py
@@ -0,0 +1,10 @@
+print"my name is Francisca"
+print "I come from Accra"
+myName ="Francisca Ekuban"
+print myName * 500
+bothStudents ="Kofi"+'Yaw'
+bothStudents = " "
+bothStudents = 'are'
+bothStudents =" "
+bothStudents = 'home'
+print bothStudents
