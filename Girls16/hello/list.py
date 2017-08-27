90cee03e0bac315426b6cf20b5cb646836e26400
EliteGirls <31381682+EliteGirls@users.noreply.github.com>
2017-08-27 09:05:18 +0000

    Add files via upload

---
+++ Girls16/hello/list.py
@@ -0,0 +1,11 @@
+print "apple"
+print "pineapples"
+print "mango"
+print "orange"
+print "grapes"
+print "strawberry"
+print "pawpaw"
+print "banana"
+print "watermelon"
+print "lemon"
+
---
+++ Girls16/photos/hello.py
@@ -0,0 +1,7 @@
+from termcolor import colored
+
+
+print colored("hello linda","blue")
+
+
+print colored ("agiss community","pink") 
